e10fd013bda790439cb64af04c39353230a5bd15
Stål <48680589+Staal22@users.noreply.github.com>
2022-04-26 18:30:00 +0200

    color select+++++

--- Source/racing_game/CharacterDisplay.h
+++ Source/racing_game/CharacterDisplay.h
@@ -44,6 +44,9 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void ChangeCharacter();
 
+	UFUNCTION(BlueprintCallable)
+	void ChangeMaterial();
+
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UStaticMesh* Character1Mesh;
@@ -51,11 +54,45 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UStaticMesh* Character2Mesh;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterial* White;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterial* Black;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterial* Yellow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterial* Blue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterial* Orange;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UMaterial* Red;
+
+	UPROPERTY(EditAnywhere)
+	UMaterial* V1BaseMat;
+
+	UPROPERTY(EditAnywhere)
+	UMaterial* V2BaseMat;
+	
 	UPROPERTY(BlueprintReadOnly)
 	TArray<FCharacterVehicle> Characters;
 
 	UPROPERTY(BlueprintReadOnly)
+	TArray<UMaterial*> Materials;
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<UMaterial*> BaseMaterials;
+
+	UPROPERTY(BlueprintReadOnly)
 	int CharacterIndex = 0;
+
+	UPROPERTY(BlueprintReadOnly)
+	int MaterialIndex = 0;
+	
 private:
 	bool bBounceUp = true;
 	
